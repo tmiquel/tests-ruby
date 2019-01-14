204f30d0b247fbb1ea51598b0fa48aabee53a805
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-01-14 15:42:02 +0100

    Finished 04_simon_says.rb

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -6,5 +6,19 @@ def shout(input)
     return input.upcase
 end
 
-def
+def repeat(input, repeat_times=2)
+	return (input + " ")*(repeat_times - 1) + input 
+end
+
+def start_of_word(word,letters_count=1)
+	return word[0..letters_count-1]
+end
+
+def first_word(sentence)
+	return sentence.split[0]
+end
+
+def titleize(sentence)
+	little_words = ['the','a', 'an', 'and']
+	return sentence.split.map.with_index {|word, i| if (little_words.include?(word) && i!=0) then word else word.capitalize end}.join(" ")
 end
